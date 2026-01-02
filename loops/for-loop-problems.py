ddda49ae954e644663f06f15c63aca6b75806dce
prasanna <prasannabhumaraju1@gmail.com>
2026-01-02 20:47:14 +0530

    updated_forloop

--- loops/for-loop-problems.py
+++ loops/for-loop-problems.py
@@ -1,3 +1,15 @@
-n=5
+'''n=5
 for i in range(n):
-    print('rama')
+    print('rama')'''
+#check the given num is prime or not
+'''n=int(input('enter a num:'))
+if n>1:
+    for i in range(2,(n//2)+1):
+        if n%i==0:
+            print('not prime')
+            break
+    else:
+        print('prime')
+else:
+    print('not prime')'''
+
